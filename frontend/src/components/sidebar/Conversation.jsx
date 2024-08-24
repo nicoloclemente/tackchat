8c6d85dcdcf2ea650c1706ce9286ea03353e70cc
Nicolò Clemente <nicoloclemente.02@gmail.com>
2024-08-24 14:07:23 +0200

    UI

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,18 +1,18 @@
 import useConversation from "../../zustand/useConversation.js";
-import {useSocketContext} from "../../context/SocketContext.jsx";
+import { useSocketContext } from "../../context/SocketContext.jsx";
 
-const Conversation = ({conversation, lastIdx, language}) => {
-    const {selectedConversation, setSelectedConversation} = useConversation();
+const Conversation = ({ conversation, lastIdx, language }) => {
+    const { selectedConversation, setSelectedConversation } = useConversation();
 
     const isSelected = selectedConversation?._id === conversation._id;
-    const {onlineUsers} = useSocketContext();
+    const { onlineUsers } = useSocketContext();
     const isOnline = onlineUsers.includes(conversation._id);
 
-    return <>
-        <div className={`flex gap-4 items-center hover:bg-gray-100 rounded p-2 py-1 cursor-pointer
-        ${isSelected ? "bg-gray-200" : ""}
-        `}
-             onClick={() => setSelectedConversation(conversation)}
+    return (
+        <div
+            className={`flex gap-4 items-center hover:bg-gray-100 rounded p-2 py-1 cursor-pointer
+            ${isSelected ? "bg-gray-200" : ""}`}
+            onClick={() => setSelectedConversation(conversation)}
         >
             <div className={`avatar ${isOnline ? "online" : ""}`}>
                 <div className="w-16 md:w-12 rounded-full">
@@ -21,18 +21,25 @@ const Conversation = ({conversation, lastIdx, language}) => {
             </div>
 
             <div className="flex flex-col flex-1 border-b pb-5">
-                <div className="flex gap-3 justify-between">
+                <div className="flex justify-between items-baseline">
                     <div className="flex flex-col overflow-hidden">
-                        <p className="font-bold text-xl md:font-normal md:text-lg text-black whitespace-nowrap overflow-hidden text-ellipsis">{conversation.fullName}</p>
-                        <p className="font-normal text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis">{isOnline ? "online" : "offline"}</p>
+                        <div className="flex items-baseline">
+                            <p className="font-bold text-xl md:font-normal md:text-lg text-black whitespace-nowrap overflow-hidden text-ellipsis">
+                                {conversation.fullName}
+                            </p>
+                            <span className="ml-2 font-normal text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis">
+                                [{conversation.username}]
+                            </span>
+                        </div>
+                        <p className="font-normal text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis">
+                            {isOnline ? "online" : "offline"}
+                        </p>
                     </div>
-                    <span className="pl-5 text-sm text-gray-500">{language}</span>
+                    {/*<span className="pl-5 text-gray-500">{language}</span>*/}
                 </div>
-                {/*{!lastIdx && <div className="divider my-0 py-0 h-1" />}*/}
             </div>
         </div>
-
-
-    </>;
+    );
 };
+
 export default Conversation;
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchInput from "./SearchInput.jsx";
 import Conversations from "./Conversations.jsx";
 import LogoutButton from "./LogoutButton.jsx";
+import { IoMdChatbubbles } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
+import { BiWorld } from "react-icons/bi";
+import { useAuthContext } from "../../context/AuthContext.jsx";
 
 const Sidebar = ({ onConversationClick }) => {
+    const {authUser} = useAuthContext();
+
+    const [selected, setSelected] = useState('chat');
+
+    const handleSelect = (button) => {
+        setSelected(button);
+    };
+
     return (
         <div className="w-full h-full md:w-auto md:h-auto bg-white md:border-r border-slate-500 p-4 flex flex-col pb-7 md:pb-4">
-            <h1 className="p-2 py-1 font-bold text-3xl mb-3">Chat</h1>
+            <h1 className="p-2 py-1 font-bold text-3xl mb-3">Chats</h1>
             <SearchInput />
             <div className="divider px-3"></div>
-            <Conversations onConversationClick={onConversationClick} />
-            <LogoutButton />
+
+            {/* Visualizza Conversations sempre nella vista desktop */}
+            <div className="hidden md:block">
+                <Conversations onConversationClick={onConversationClick} />
+            </div>
+
+            {/* Visualizza Conversations solo se discover è selezionato su mobile */}
+            {selected === 'discover' && (
+                <div className="md:hidden">
+                    <Conversations onConversationClick={onConversationClick} />
+                </div>
+            )}
+
+            {selected === 'chats' && (
+                <div className="md:hidden">
+                    <Conversations onConversationClick={onConversationClick} />
+                </div>
+            )}
+
+            {selected === 'profile' && (
+                <div className="flex items-center justify-center w-full h-full md:hidden">
+                    <div
+                        className="px-4 text-center text-xl text-orange-600 font-semibold flex flex-col items-center gap-2">
+                        <div className="w-60 rounded-full">
+                            <img src={authUser.profilePic} alt="user avatar"/>
+                        </div>
+                        <p>Name: {authUser.fullName}</p>
+                        <p>Username: {authUser.username}</p>
+                    </div>
+                </div>
+            )}
+
+            <div className="hidden md:flex md:flex-col md:mt-auto">
+                <LogoutButton/>
+            </div>
+
+            <div className="md:hidden flex justify-around bg-gray-100 border rounded-full mt-auto p-2 text-4xl">
+                <div
+                    className={`flex flex-col items-center ${selected === 'discover' ? 'text-orange-600' : ''}`}
+                    onClick={() => handleSelect('discover')}
+                >
+                    <BiWorld/>
+                    <span className="text-xs">Discover</span>
+                </div>
+                <div
+                    className={`flex flex-col items-center ${selected === 'chats' ? 'text-orange-600' : ''}`}
+                    onClick={() => handleSelect('chats')}
+                >
+                    <IoMdChatbubbles />
+                    <span className="text-xs">Chats</span>
+                </div>
+                <div
+                    className={`flex flex-col items-center ${selected === 'profile' ? 'text-orange-600' : ''}`}
+                    onClick={() => handleSelect('profile')}
+                >
+                    <FaUser />
+                    <span className="text-xs">Profile</span>
+                </div>
+            </div>
         </div>
     );
 };
